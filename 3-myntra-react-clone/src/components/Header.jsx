64c88edee05f26c7f88a960b1064c6a4c2a5469b
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2024-04-06 11:56:47 +0530

    Ourpage layout  completed and state management in not complete

--- 3-myntra-react-clone/src/components/Header.jsx
+++ 3-myntra-react-clone/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import { FcBusinessman } from "react-icons/fc";
+import { FaHeart } from "react-icons/fa";
+import { IoBagAdd } from "react-icons/io5";
+import {Link} from "react-router-dom"
 const Header = () => {
   return (
     <>
       <header>
         <div className="logo_container">
-          <a href="#">
+          <Link to="/">
             <img
               className="myntra_home"
               src="images/myntra_logo.webp"
               alt="Myntra Home"
             />
-          </a>
+          </Link>
         </div>
         <nav className="nav_bar">
           <a href="#">Men</a>
@@ -36,13 +39,15 @@ const Header = () => {
           </div>
 
           <div className="action_container">
+            <FaHeart />
             <span className="action_name">Wishlist</span>
           </div>
 
-          <a className="action_container" href="pages/bag.html">
+          <Link className="action_container" to="/bag">
+            <IoBagAdd />
             <span className="action_name">Bag</span>
             <span className="bag-item-count">0</span>
-          </a>
+          </Link>
         </div>
       </header>
     </>
--- 3-myntra-react-clone/src/main.jsx
+++ 3-myntra-react-clone/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Home from "./routes/Home.jsx";
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import Bag from "./routes/Bag.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home /> /*loader: postLoader*/,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+        /*action:createPostAction,*/
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
